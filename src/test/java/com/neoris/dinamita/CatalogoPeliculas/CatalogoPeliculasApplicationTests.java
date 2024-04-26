1fa69a60bd06f9ce33ec49c85940718a436e2eb7
Marco Aguirre <marco.aguirre@neoris.com>
2024-04-26 13:21:40 -0600

    Creacion y configuracion del proyecto

---
+++ src/test/java/com/neoris/dinamita/CatalogoPeliculas/CatalogoPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoris.dinamita.CatalogoPeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
